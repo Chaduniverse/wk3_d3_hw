e03ca25e1bcbb5936d100c640bb98f7765434dfe
Shannon Melvin <Shannonmelvin88@gmail.com>
2023-02-09 18:19:10 -0500

    week 3 assignment

---
+++ areacirmodule.py
@@ -0,0 +1,14 @@
+def  calculate_area(length,height):
+    """display simple formula """
+    area = f'{length} * {height}'   
+
+
+
+
+def calculate_circumference(radius,pi=3.14):
+    """display formula""" 
+    C = f'int(2) * {radius} * {pi}' 
+
+
+
+
